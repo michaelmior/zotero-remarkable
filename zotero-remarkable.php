c417de6aa4c9f78161bf8bcd61c67b00b2701b73
Michael Mior <mmior@uwaterloo.ca>
2018-03-17 20:55:59 -0400

    Add working version of sync script

---
+++ zotero-remarkable.php
@@ -0,0 +1,91 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use splitbrain\RemarkableAPI\RemarkableAPI;
+use splitbrain\RemarkableAPI\RemarkableFS;
+
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
+$user = getenv('ZOTERO_USER');
+$zoteroKey = getenv('ZOTERO_API_KEY');
+$collection = getenv('ZOTERO_COLLECTION');
+$webdavUrl = getenv('WEBDAV_URL');
+$webdavAuth = getenv('WEBDAV_AUTH');
+$reMarkableToken = getenv('REMARKABLE_TOKEN');
+
+$api = new RemarkableAPI();
+$api->init($reMarkableToken);
+$fs = new RemarkableFS($api);
+$parent = $fs->mkdirP("/Zotero");
+
+// Zotero API client
+$client = new GuzzleHttp\Client([
+    'base_uri' => 'https://api.zotero.org/users/'. $user . '/',
+    'headers' => [
+        'Zotero-API-Version' => 3,
+        'Zotero-API-Key' => $zoteroKey,
+        'Content-Type' => 'application/json'
+    ]
+]);
+
+// WebDAV HTTP client for downloading zips
+$webDAVClient = new GuzzleHttp\Client([
+    'base_uri' => $webdavUrl,
+    'auth' => explode(':', $webdavAuth)
+]);
+
+$zipper = new \Chumper\Zipper\Zipper;
+$tmp_dir = sys_get_temp_dir();
+
+$to_process = [];
+$titles = [];
+$collections = [];
+$versions = [];
+$response = $client->get('collections/' . $collection . '/items');
+foreach (json_decode($response->getBody()) as $item) {
+    // Store data to access parent info later
+    $titles[$item->data->key] = $item->data->title;
+    if (property_exists($item->data, 'collections')) {
+        $collections[$item->data->key] = $item->data->collections;
+    }
+    $versions[$item->data->key] = $item->data->version;
+
+    // Only upload PDF attachments
+    if ($item->data->itemType == 'attachment' && $item->data->contentType == 'application/pdf') {
+        $to_process[] = $item;
+    }
+}
+
+$to_remove = [];
+foreach ($to_process as $item) {
+    echo $item->data->key . "\n";
+
+    // Store data for future removal
+    $parentItem = $item->data->parentItem;
+    $title = $titles[$parentItem];
+    $to_remove[] = [
+        'key' => $parentItem,
+        'version' => $versions[$parentItem],
+        'collections' => array_diff($collections[$parentItem], [$collection])
+    ];
+
+    // Download the zip file from WebDAV
+    $zip_file = $item->data->key . '.zip';
+    $tmp_file = $tmp_dir . '/' . $zip_file;
+    $zip_resp = $webDAVClient->get($zip_file, ['save_to' => $tmp_file]);
+
+    // Extract the PDF, upload to reMarkable and delete locally
+    $zip = $zipper->make($tmp_file);
+    $content = $zip->getFileContent($item->data->filename);
+    $api->uploadPDF($content, $title, $parent);
+    unlink($tmp_file);
+}
+
+// Remove the items from the collection
+foreach (array_chunk($to_remove, 50) as $remove_chunk) {
+    $client->post('items', ['body' => json_encode($remove_chunk)]);
+}
+
+?>
